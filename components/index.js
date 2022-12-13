6c966b0928404bbd028d8739c0348b002e74cbb7
Septian Maulana <septianm028@gmail.com>
2022-12-14 00:47:40 +0700

    feat: added create NFT page and config adjustments

--- components/index.js
+++ components/index.js
@@ -5,5 +5,6 @@ import Banner from 'components/Banner';
 import CreatorCard from 'components/CreatorCard';
 import SearchBar from 'components/SearchBar';
 import NFTCard from 'components/NFTCard';
+import Input from 'components/Input';
 
-export { Navbar, Footer, Button, Banner, CreatorCard, SearchBar, NFTCard };
+export { Navbar, Footer, Button, Banner, CreatorCard, SearchBar, NFTCard, Input };
