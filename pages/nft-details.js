2aa7ffe0990ed2143fd04d287e3f5d590ee389a0
Septian Maulana <septianm028@gmail.com>
2023-01-02 19:06:59 +0700

    feat: created resell NFT

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -200,6 +200,15 @@ const NFTDetails = () => {
             <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-base border border-gray p-2">
               You cannot buy your own NFT
             </p>
+          ) : currentAccount === nft.owner.toLowerCase() ? (
+            <Button
+              btnName="List on Marketplace"
+              btnType="primary"
+              classStyles="mr-5 sm:mr-0 sm:mb-5 rounded-xl"
+              handleClick={() =>
+                router.push(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`)
+              }
+            />
           ) : (
             <Button
               btnName={`Buy for ${nft.price} ${nftCurrency}`}
